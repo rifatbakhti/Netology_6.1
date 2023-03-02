53d91b0337dce9abb19cfa513043c74a38b805c5
Рифат Бахтиев <rifatbakhtiev@gmail.com>
2023-03-02 17:18:26 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from salary import salary_staff
+from people import staff
+import datetime
+
+
+def get_salary():
+    for key, value in salary_staff.items():
+        print(key, value)
+
+def get_staff():
+    for person in staff:
+        print(person)
+
+def main():
+    today = datetime.datetime.today()
+    print(today.strftime('%d/%m/%Y'))
+
+    option = input(f'Введите команду 1, чтобы получить сотрудников с ЗП и 2, чтобы получить список сотрудников: ')
+    if option == "1":
+        get_salary()
+    elif option == "2":
+        get_staff()
+
+    else:
+        print("Не работает!")
+
+if __name__ == '__main__':
+    main()
